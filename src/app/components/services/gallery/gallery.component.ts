8de0adb529ad7a6b5a72c9995cf9ba27fae1f8e9
UselessMOS0 <90761312+UselessMOS0@users.noreply.github.com>
2024-02-29 16:22:03 +0100

    b

--- src/app/components/services/gallery/gallery.component.ts
+++ src/app/components/services/gallery/gallery.component.ts
@@ -12,15 +12,23 @@ export class GalleryComponent {
   id !: Myrange
   page : number = 0
   rows : number = 5
+  max_n !: number
 
   constructor(public s : LanguageService,private route: ActivatedRoute){
     this.route.params.subscribe(params => {
         this.id = +params['$id'] as Myrange
     })
+    this.max_n = this.s.getString(`content${this.id}`,'projects').length - 1
+    console.log(this.max_n)
+    console.log(this.s.getString(`content${this.id}`,'projects')[this.max_n])
   }
 
   getString() :  any{
     return this.s.getString(`content${this.id}`,'projects').slice(this.page * this.rows, this.rows + (this.page * this.rows))
+  }
+
+  getImage(n : number) : any{
+    return this.s.getString(`content${this.id}`,'projects')[n]
   }
 
   changePage(num : number){
@@ -34,7 +42,7 @@ export class GalleryComponent {
     return this.page + num < 0
   }
   checkOver(num : number){
-    return this.page + num >= Math.ceil(this.s.getString(`content${this.id}`,'projects').length / this.rows)
+    return this.page + num >= Math.ceil(this.s.getString(`content${this.id}`,'projects').length / (this.rows * 2))
   }
   scrollTop() {
     window.scroll(0, 0);
