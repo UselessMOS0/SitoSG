65afc577c6294144e4d465047d3dc881d5570ee0
UselessMOS0 <90761312+UselessMOS0@users.noreply.github.com>
2024-04-19 17:39:19 +0200

    as

--- src/app/components/services/content/content.component.ts
+++ src/app/components/services/content/content.component.ts
@@ -1,50 +1,87 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { Subscription, interval } from 'rxjs';
 import { LanguageService } from 'src/app/language.service';
 
-export type Myrange = 1 | 2 | 3 | 4 | 5
+export type Myrange = 1 | 2 | 3 | 4 | 5;
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
+export class ContentComponent implements AfterViewInit {
+  id!: Myrange;
 
-export class ContentComponent {
-  id !: Myrange
-  constructor(public s : LanguageService,private route: ActivatedRoute){
-    this.route.params.subscribe(params => {
-        this.id = +params['$id'] as Myrange
-    })
+  currentImage: string = '';
+  nextImage: string = '';
+
+  @ViewChild('current') currentImg!: ElementRef<HTMLImageElement>;
+
+  subscription!: Subscription;
+
+  updateCarousel() {
+    const projects = this.s.getString(`content${this.id}`, 'projects');
+    const proj = projects[Math.floor(Math.random() * projects.length)];
+    const source = interval(5);
+    let animationSubscr!: Subscription;
+
+    animationSubscr = source.subscribe((v) => {
+      const originalOp = this.currentImg.nativeElement.style.opacity;
+      let op = originalOp.trim() === '' ? 1 : parseFloat(originalOp);
+      op -= 0.01;
+      this.currentImg.nativeElement.style.opacity = op.toString();
+      if (op <= 0) {
+        this.currentImage = this.nextImage;
+        // @ts-ignore
+        this.nextImage = proj.thumbnail;
+        this.currentImg.nativeElement.style.opacity = '1';
+        animationSubscr.unsubscribe();
+      }
+    });
   }
-  getTitle(){
-    return this.s.getString(`content${this.id}`,'title') as string
+
+  loadInitialImages() {
+    const projects = this.s.getString(`content${this.id}`, 'projects');
+    this.currentImage =
+      // @ts-ignore
+      projects[Math.floor(Math.random() * projects.length)].thumbnail;
+    this.nextImage =
+      // @ts-ignore
+      projects[Math.floor(Math.random() * projects.length)].thumbnail;
   }
-  getText(){
-    let str = this.s.getString(`content${this.id}`,'text') as string
-    
-    return str.split('/n')
+
+  ngAfterViewInit(): void {
+    this.createInterval();
   }
-  getProjects():  any {
-    if (this.id < 5) {
-      console.log(this.s.getString(`content${this.id}`,'projects'))
-      let unshuffled = this.s.getString(`content${this.id}`,'projects')
-      console.log('1' ,unshuffled)
-      let shuffled = this.shuffle(unshuffled)
-      console.log('2' ,unshuffled)
-      return shuffled
-  
-    }
-    else {
-      return this.s.getString(`content${this.id}`,'projects')
-    }
-    
+
+  constructor(public s: LanguageService, private route: ActivatedRoute) {
+    this.route.params.subscribe((params) => {
+      this.id = +params['$id'] as Myrange;
+      this.loadInitialImages();
+    });
   }
+
+  createInterval() {
+    const source = interval(5000);
+    this.subscription = source.subscribe((v) => this.updateCarousel());
+  }
+
+  getTitle() {
+    return this.s.getString(`content${this.id}`, 'title') as string;
+  }
+
+  getText() {
+    let str = this.s.getString(`content${this.id}`, 'text') as string;
+
+    return str.split('/n');
+  }
+
   scrollTop() {
     window.scroll(0, 0);
   }
-  shuffle (array: any) { 
-    let sorted = array.sort(() => Math.random() - 0.5); 
-    return sorted
-}; 
+  shuffle(array: any) {
+    let sorted = [...array].sort(() => Math.random() - 0.5);
+    return sorted;
+  }
 }
